ec037bf0419629c48072cdbb766639eead9c84d0
lovelynhok <phanleminhlong0908@gmail.com>
2024-04-08 09:22:49 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, ImageBackground, Image, TextInput, TouchableOpacity, Text, Alert } from 'react-native';
+
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    Alert.alert(' Đăng nhập thành công với ' + email , ' password ' + password ) 
+  };
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={{ uri: 'C:\workspace\AwesomeProject\assets\images.jpeg' }} style={styles.backgroundImage}>
+        <View style={styles.logoContainer}>
+          <Image source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar3.png' }} style={styles.logo} />
+        </View>
+        <View style={styles.formContainer}>
+          <View style={styles.card}>
+            <TextInput
+              placeholder="Email"
+              style={styles.input}
+              value={email}
+              onChangeText={setEmail}
+            />
+          </View>
+          <View style={styles.card}>
+            <TextInput
+              placeholder="Password"
+              secureTextEntry={true}
+              style={styles.input}
+              value={password}
+              onChangeText={setPassword}
+            />
+          </View>
+          <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+            <Text style={styles.loginButtonText}>Log In</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginTop: 160,
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    borderRadius:60,
+  },
+  formContainer: {
+    marginHorizontal: 20,
+    marginTop: 50,
+    padding:20,
+    borderRadius:10,
+    backgroundColor:'rgba(255, 255, 255, 0.3)'
+  },
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    marginBottom: 20,
+    padding:10,
+  },
+  input: {
+    height: 40,
+    paddingHorizontal: 10,
+    borderBottomWidth:1,
+    borderBottomColor:'#B0C4DE'
+  },
+  loginButton: {
+    backgroundColor: '#7B68EE',
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
+
+export default LoginScreen;
